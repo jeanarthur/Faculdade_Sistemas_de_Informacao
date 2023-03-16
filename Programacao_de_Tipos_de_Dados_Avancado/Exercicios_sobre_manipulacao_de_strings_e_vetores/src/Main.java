949c81f7f5dacaba70573f8a4ad998553f860f1a
Jean Arthur <jeanarthursantosalmeida@gmail.com>
2023-03-15 21:58:39 -0300

    Adiciona solução para novos exercícios

---
+++ Programacao_de_Tipos_de_Dados_Avancado/Exercicios_sobre_manipulacao_de_strings_e_vetores/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Methods.removeDuplicatedChars("Abacccaaxi"));
+        System.out.println(Methods.duplicateVowels("Abacaxi"));
+        System.out.println(Methods.removeOddChars("Arara"));
+        System.out.println(Methods.alternateCase("Abacaxi"));
+        System.out.println(Methods.groupRepeatedChars("Abacaxi"));
+        System.out.println(Methods.sortChars("Abacaxi"));
+        System.out.println(Methods.sortWords("Parece que deu certo!"));
+    }
+}
