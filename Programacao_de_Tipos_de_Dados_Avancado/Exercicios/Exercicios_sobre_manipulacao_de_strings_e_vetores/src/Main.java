0734633b0def35105b0b9ce1b8aec8c5a1d8be93
jeanarthur <jean.arthursa@gmail.com>
2023-03-18 23:08:56 -0300

    adiciona soluções restantes para os exercicios

--- Programacao_de_Tipos_de_Dados_Avancado/Exercicios/Exercicios_sobre_manipulacao_de_strings_e_vetores/src/Main.java
+++ Programacao_de_Tipos_de_Dados_Avancado/Exercicios/Exercicios_sobre_manipulacao_de_strings_e_vetores/src/Main.java
@@ -6,6 +6,11 @@ public class Main {
         System.out.println(Methods.alternateCase("Abacaxi"));
         System.out.println(Methods.groupRepeatedChars("Abacaxi"));
         System.out.println(Methods.sortChars("Abacaxi"));
-        System.out.println(Methods.sortWords("Parece que deu certo!"));
+        System.out.println(Methods.sortWords("Ontem de antes"));
+        System.out.println(Methods.isValidCNH("58672907551") ? "CNH valida" : "CNH invalida");
+        System.out.println(Methods.isValidCNH("58672907559") ? "CNH valida" : "CNH invalida");
+        String[] articlesPrepositions = {"a", "ante", "após", "até", "com", "contra", "de", "desde", "em", "entre", "para", "perante", "por", "sem", "sob", "sobre", "trás", "o", "a", "os", "as", "um", "uma", "uns", "umas", "ao", "à", "aos", "às", "do", "da", "dos", "das", "no", "na", "nos", "nas", "pelo", "pela", "pelos", "pelas", "num", "numa", "nuns", "numas"};
+        System.out.println(Methods.sortWordsButIgnore("O livro esta em cima da mesa", articlesPrepositions));
+        System.out.println(Methods.getAllowedWords("O livro esta em cima da mesa", articlesPrepositions));
     }
 }
--- Programacao_de_Tipos_de_Dados_Avancado/Exercicios/Exercicios_sobre_manipulacao_de_strings_e_vetores/src/Methods.java
+++ Programacao_de_Tipos_de_Dados_Avancado/Exercicios/Exercicios_sobre_manipulacao_de_strings_e_vetores/src/Methods.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Methods {
 
+    // Exercicio 1
     public static String removeDuplicatedChars(String string) {
         for(Character c : string.toCharArray()){
             string = string.toLowerCase().replaceAll(String.format("%s{2,}", c), "#");
@@ -9,6 +12,7 @@ public class Methods {
         return string.replaceAll("#", "");
     }
 
+    // Exercicio 2
     public static String duplicateVowels(String string){
         String result = "";
         for (Character c : string.toCharArray()){
@@ -17,6 +21,7 @@ public class Methods {
         return result;
     }
 
+    // Exercicio 3
     public static String removeOddChars(String string){
         for(Character c : string.toCharArray()){
             string = string.toLowerCase().replaceAll(String.format("[^%s]", c), "").length() % 2 != 0 ?
@@ -25,6 +30,7 @@ public class Methods {
         return string;
     }
 
+    // Exercicio 4
     public static String alternateCase(String string){
         String result = "";
         for(int i = 0; i < string.length(); i++){
@@ -33,6 +39,7 @@ public class Methods {
         return result;
     }
 
+    // Exercicio 5
     public static String groupRepeatedChars(String string){
         String result = "";
         for(Character c : string.toLowerCase().toCharArray()){
@@ -46,16 +53,78 @@ public class Methods {
         return result;
     }
 
+    // Exercicio 6
     public static String sortChars(String string){
         char[] result = string.toCharArray();
         Arrays.sort(result);
         return Arrays.toString(result).replaceAll("[\\[\\],\\s]", "");
     }
 
+    // Exercicio 7
     public static String sortWords(String string){
-        String[] words = string.split("\\s");
+        String[] words = string.toLowerCase().split("\\s");
         Arrays.sort(words);
-        return Arrays.toString(words).replaceAll("\\[\\],", "");
+        return Arrays.toString(words).replaceAll("[\\[\\],]", "");
     }
 
+    // Exercicio 8
+    public static Boolean isValidCNH(String cnh){
+        if (cnh.length() != 11) { return false; }
+
+        int sum = 0;
+        for (int i = 0, j = 9; i < 9;) { sum += Integer.parseInt(cnh.substring(i, ++i)) * j--; }
+
+        int firstDigit = (sum % 11 > 9) ? 0 : sum % 11;
+        if (Integer.parseInt(cnh.substring(9, 10)) != firstDigit) { return false; }
+
+        int discount = (sum % 11 > 9) ? 2 : 0;
+
+        sum = 0;
+        for (int i = 0, j = 1; i < 9;) {
+            sum += Integer.parseInt(cnh.substring(i, ++i)) * j++;
+        }
+
+        int secondDigit = (sum % 11 > 9) ? 0 : (sum % 11) - discount;
+        return Integer.parseInt(cnh.substring(10, 11)) == secondDigit;
+    }
+
+    // Exercicio 9
+    public static String sortWordsButIgnore(String string, String[] toIgnore){
+        String[] words = string.toLowerCase().split("\\s");
+        String toSort = string.toLowerCase();
+        String result = "";
+
+        for(String word : words){
+            if (Arrays.asList(toIgnore).contains(word)){
+                result += "\033[36m" + word + "\033[0m ";
+                toSort = toSort.replaceFirst(word, "");
+            } else {
+                if (!result.contains("@")){ result += "@ "; }
+                else { result += "# "; }
+            }
+        }
+
+        words = toSort.split("\\s");
+        Arrays.sort(words);
+        for(String word : words){
+            if (word.isBlank()) { continue; }
+            result = result.replaceFirst("@", word);
+            result = result.replaceFirst("#", "@");
+        }
+
+        return result;
+    }
+
+    // Exercicio 10
+    public static String getAllowedWords(String string, String[] allowedWords) {
+        String[] words = string.toLowerCase().split("\\s");
+        String result = "";
+
+        for (String word : words) {
+            if (Arrays.asList(allowedWords).contains(word)) {
+                result += word + " ";
+            }
+        }
+        return result;
+    }
 }
