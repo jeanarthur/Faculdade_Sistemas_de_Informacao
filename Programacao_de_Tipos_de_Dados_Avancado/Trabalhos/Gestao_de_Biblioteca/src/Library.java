617218beb4e6e12892a1200fad77f9a212ecab28
jeanarthur <jean.arthursa@gmail.com>
2023-04-02 23:07:26 -0300

    Refatora código
    Altera o uso de ArrayList para vetor de String
    ALtera métodos da classe Csv

--- Programacao_de_Tipos_de_Dados_Avancado/Trabalhos/Gestao_de_Biblioteca/src/Library.java
+++ Programacao_de_Tipos_de_Dados_Avancado/Trabalhos/Gestao_de_Biblioteca/src/Library.java
@@ -1,18 +1,25 @@
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Library {
-    static List<Book> books = new ArrayList<>();
     private static final String DATABASE_PATH = "Programacao_de_Tipos_de_Dados_Avancado/Trabalhos/Gestao_de_Biblioteca/src/database.csv";
     private static final String REPORT_PATH= "Programacao_de_Tipos_de_Dados_Avancado/Trabalhos/Gestao_de_Biblioteca/reports/";
+    static String[] books;
     static String searchedBy = "";
 
     public static void main(String[] args) {
         boolean isRunning = true;
-        books = Book.parseBook(Csv.read(DATABASE_PATH));
+        try {
+            String[] database = Csv.getContent(DATABASE_PATH);
+            books = new String[database.length - 1];
+            for(int i = 0; i < database.length - 1; i++) {
+                books[i] = database[i+1];
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar a base de dados! Erro: " + e.getMessage());
+        }
 
         do{
             String[] menuOptions = {
@@ -32,46 +39,66 @@ public class Library {
                     case 3 -> search();
                     case 4 -> generateReport(searchBooks());
                     case 5 -> isRunning = false;
-                    default -> throw new Exception();
+                    default -> System.out.println("\u001B[31mOpção inválida!\u001B[0m");
                 }
-            } catch (Exception ex) {
-                System.out.println("\u001B[31mOpção inválida!\u001B[0m");
+            } catch (NumberFormatException ex) {
+                System.out.println("\u001B[31mOpção informada precisa ser um número!\u001B[0m");
+            } catch (Exception ex){
+                System.out.println("\u001B[31mErro inesperado!\u001B[0m\nErro: " + ex.getMessage());
             }
         } while(isRunning);
     }
 
-    private static void generateReport(List<Book> books) {
-        if (books.size() != 0){
+    private static void generateReport(String[] books) {
+        if (books.length != 0){
             LocalDateTime date = LocalDateTime.now();
             DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss");
-            List<String[]> booksRecords = Book.toCsvPattern(books);
-
             String filename = String.format("booksReportBy%s_%s.csv", searchedBy, date.format(dateFormatter));
-            Csv.write(booksRecords, REPORT_PATH + filename);
+
+            try {
+                Csv.addRow(REPORT_PATH + filename, "nome_do_livro,numero_de_paginas,nome_do_autor,area_de_interesse");
+            } catch (IOException e) {
+                System.out.println("Erro ao adicionar linha no relatório! Erro: " + e.getMessage());
+            }
+            for (String book : books){
+                try {
+                    Csv.addRow(REPORT_PATH + filename, book);
+                } catch (IOException e) {
+                    System.out.println("Erro ao adicionar linha no relatório! Erro: " + e.getMessage());
+                }
+            }
+
             System.out.printf("\u001B[32mRelatório gerado com sucesso!\u001B[0m\nCaminho do arquivo: %s\n", REPORT_PATH + filename);
         }
     }
 
     public static void search(){
-        if (books.size() == 0){ System.out.println("Nenhum livro registrado no sistema!"); }
+        if (books.length == 0){ System.out.println("Nenhum livro registrado no sistema!"); }
         else {
-            List<Book> foundBooks = searchBooks();
+            String[] foundBooks = searchBooks();
             while (true){
-                if (foundBooks.size() == 0){ break; }
-                List<String> booksList = listBooks(foundBooks);
-                booksList.add("Gerar Relatório da busca");
-                booksList.add("Remover um dos livros");
-                booksList.add("Voltar");
-                printMenu("Livros", booksList.toArray(new String[0]));
+                if (foundBooks.length == 0){ break; }
+                String[] booksList = listBooks(foundBooks);
+                String[] options = new String[booksList.length + 3];
+                int i = 0;
+                for (String book : booksList){ options[i++] = book; }
+                options[i++] = ("Gerar Relatório da busca");
+                options[i++] = ("Remover um dos livros");
+                options[i] = ("Voltar");
+                printMenu("Livros", options);
                 String option = getInput("Digite uma opção: ");
 
                 try {
                     int optionValue = Integer.parseInt(option);
-                    if (optionValue == foundBooks.size() + 1) { generateReport(foundBooks); }
-                    else if (optionValue == foundBooks.size() + 2) { foundBooks = remove(foundBooks); }
-                    else if (optionValue == foundBooks.size() + 3) { break; }
-                    else if (optionValue < 1 || optionValue > foundBooks.size()) { throw new Exception(); }
-                } catch (Exception ex) { System.out.println("\u001B[31mOpção inválida!\u001B[0m"); System.out.println(ex.getMessage()); }
+                    if (optionValue == foundBooks.length + 1) { generateReport(foundBooks); }
+                    else if (optionValue == foundBooks.length + 2) { foundBooks = remove(foundBooks); }
+                    else if (optionValue == foundBooks.length + 3) { break; }
+                    else if (optionValue < 1 || optionValue > foundBooks.length) { System.out.println("\u001B[31mOpção inválida!\u001B[0m"); }
+                } catch (NumberFormatException ex) {
+                    System.out.println("\u001B[31mOpção informada precisa ser um número!\u001B[0m");
+                } catch (Exception ex){
+                    System.out.println("\u001B[31mErro inesperado!\u001B[0m\nErro: " + ex.getMessage());
+                }
 
             }
         }
@@ -84,97 +111,150 @@ public class Library {
         String area = getInput("Área de interesse: ");
         try {
             int pages = Integer.parseInt(getInput("Quantidade de páginas: "));
-            books.add(new Book(name, pages, author, area));
-            updateDatabase();
+            String bookRecord = String.format("%s,%d,%s,%s", name, pages, author, area);
+            books = Csv.addRow(DATABASE_PATH, bookRecord);
             System.out.printf("\u001B[32mLivro: %s adicionado com sucesso!\u001B[0m\n", name);
         } catch (Exception ex) { System.out.println("\u001B[31mErro ao cadastrar informações!\u001B[0m"); }
     }
 
-    private static List<Book> remove(List<Book> bookList) {
-        if (bookList.size() == 0){ return new ArrayList<>(); }
-        if (bookList.size() == 1){
-            Book book = bookList.get(0);
-            books.remove(book);
-            System.out.printf("\u001B[32mLivro %s removido com sucesso!\u001B[0m\n", book.getName());
-            return new ArrayList<>();
+    private static String[] remove(String[] bookList) {
+        if (bookList.length == 0){ return new String[0]; }
+        String[] newBookList = new String[bookList.length - 1];
+        if (bookList.length == 1){
+            try {
+                Csv.removeRow(DATABASE_PATH, bookList[0]);
+            } catch (IOException e) {
+                System.out.println("Erro ao remover da base de dados! Erro: " + e.getMessage());
+            }
+            System.out.printf("\u001B[32mLivro %s removido com sucesso!\u001B[0m\n", bookList[0].split(",")[0]);
+            return new String[0];
         } else {
             boolean isSelecting = true;
             do{
-                List<String> booksList = listBooks(bookList);
-                booksList.add("Cancelar");
-                printMenu("Remover livro nº: ", booksList.toArray(new String[0]));
+                String[] booksList = listBooks(bookList);
+                String[] options = new String[bookList.length + 1];
+                int i = 0;
+                for (String book : booksList){ options[i++] = book; }
+                options[i] = "Cancelar";
+                printMenu("Remover livro nº: ", options);
                 String option = getInput("Digite uma opção: ");
 
                 try {
                     int optionValue = Integer.parseInt(option);
-                    if (optionValue == bookList.size() + 1) { break; }
-                    if (optionValue < 1 || optionValue > bookList.size()) { throw new Exception(); }
-                    Book selectedBook = bookList.get(optionValue-1);
-                    books.remove(selectedBook);
-                    bookList.remove(selectedBook);
-                    System.out.printf("\u001B[32mLivro %s removido com sucesso!\u001B[0m\n", selectedBook.getName());
-                } catch (Exception ex) {
-                    System.out.println("\u001B[31mOpção inválida!\u001B[0m");
-                    System.out.println(ex.getMessage());
+                    if (optionValue == bookList.length + 1) { break; }
+                    if (optionValue < 1 || optionValue > bookList.length) { System.out.println("\u001B[31mOpção inválida!\u001B[0m"); }
+                    String selectedBook = bookList[optionValue-1];
+                    Csv.removeRow(DATABASE_PATH, selectedBook);
+                    i = 0;
+                    for (String book : bookList){
+                        if (!book.equals(selectedBook)){ newBookList[i++] = book; }
+                    }
+                    System.out.printf("\u001B[32mLivro %s removido com sucesso!\u001B[0m\n", selectedBook.split(",")[0]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("\u001B[31mOpção informada não é um número!\u001B[0m");
+                    continue;
+                } catch (Exception ex){
+                    System.out.println("\u001B[31mErro inesperado!\u001B[0m\nErro: " + ex.getMessage());
                     continue;
                 }
                 isSelecting = false;
             } while (isSelecting);
         }
-        updateDatabase();
-        return bookList;
+        books = newBookList;
+        return newBookList;
     }
 
-    public static List<String> listBooks(List<Book> books){
-        if (books.size() == 0){ System.out.println("Nenhum livro encontrado!"); }
+    public static String[] listBooks(String[] books){
+        if (books.length == 0){ System.out.println("Nenhum livro encontrado!"); }
         else {
-            List<String> rows = new ArrayList<>();
-            for(Book book : books){
-                rows.add(String.format("""
+            String[] rows = new String[books.length];
+            int i = 0;
+            for(String book : books){
+                String[] columns = book.split(",");
+                rows[i++] = (String.format("""
                                 Nome: %s
                                 Autor: %s
                                 Área de interesse: %s
                                 Número de páginas: %d
                                 \s
                                 """,
-                        book.getName(), book.getAuthor(), book.getArea(), book.getPages()));
+                        columns[0], columns[2], columns[3], Integer.parseInt(columns[1])));
             }
             return rows;
         }
-        return new ArrayList<>();
+        return new String[0];
     }
 
-    private static List<Book> searchByArea() {
+    private static String[] searchByArea() {
         String area = getInput("Digite a area de interesse: ");
         searchedBy = "Area_" + area;
-        List<Book> bookList = books.stream().filter(book -> book.getArea().equalsIgnoreCase(area)).toList();
-        if (bookList.size() == 0){ System.out.printf("Nenhum livro registrado com o área de interesse %s!%n", area); }
-        return bookList;
+
+        int founded = 0;
+        for (String book : books){
+            String bookArea = book.split(",")[3];
+            if (bookArea.equalsIgnoreCase(area)) { founded++; }
+        }
+
+        String[] foundedBooks = new String[founded];
+        int i = 0;
+        for (String book : books){
+            String bookArea = book.split(",")[3];
+            if (bookArea.equalsIgnoreCase(area)) { foundedBooks[i++] = book; }
+        }
+
+        if (foundedBooks.length == 0){ System.out.printf("Nenhum livro registrado na área de interesse %s!%n", area); }
+        return foundedBooks;
     }
 
-    private static List<Book> searchByAuthor() {
+    private static String[] searchByAuthor() {
         String author = getInput("Digite o nome do autor: ");
         searchedBy = "Author_" + author;
-        List<Book> bookList = books.stream().filter(book -> book.getAuthor().equalsIgnoreCase(author)).toList();
-        if (bookList.size() == 0){ System.out.printf("Nenhum livro do autor %s registrado!%n", author); }
-        return bookList;
+
+        int founded = 0;
+        for (String book : books){
+            String bookAuthor = book.split(",")[2];
+            if (bookAuthor.equalsIgnoreCase(author)) { founded++; }
+        }
+
+        String[] foundedBooks = new String[founded];
+        int i = 0;
+        for (String book : books){
+            String bookAuthor = book.split(",")[2];
+            if (bookAuthor.equalsIgnoreCase(author)) { foundedBooks[i++] = book; }
+        }
+
+        if (foundedBooks.length == 0){ System.out.printf("Nenhum livro registrado do autor %s!%n", author); }
+        return foundedBooks;
     }
 
-    private static List<Book> searchByName() {
+    private static String[] searchByName() {
         String name = getInput("Digite o título: ");
         searchedBy = "Name_" + name;
-        List<Book> bookList = books.stream().filter(book -> book.getName().equalsIgnoreCase(name)).toList();
-        if (bookList.size() == 0){ System.out.printf("Nenhum livro registrado com o título %s!%n", name); }
-        return bookList;
+
+        int founded = 0;
+        for (String book : books){
+            String bookName = book.split(",")[0];
+            if (bookName.equalsIgnoreCase(name)) { founded++; }
+        }
+
+        String[] foundedBooks = new String[founded];
+        int i = 0;
+        for (String book : books){
+            String bookName = book.split(",")[0];
+            if (bookName.equalsIgnoreCase(name)) { foundedBooks[i++] = book; }
+        }
+
+        if (foundedBooks.length == 0){ System.out.printf("Nenhum livro registrado com o título %s!%n", name); }
+        return foundedBooks;
     }
 
-    private static List<Book> searchBooks() {
-        if (books.size() == 0) {
+    private static String[] searchBooks() {
+        if (books.length == 0) {
             System.out.println("Nenhum livro registrado no sistema!");
-            return new ArrayList<>();
+            return new String[0];
         }
         boolean isSearching = true;
-        List<Book> foundBooks = new ArrayList<>();
+        String[] foundBooks = new String[0];
         do{
             String[] menuOptions = {
                     "Nome",
@@ -244,11 +324,6 @@ public class Library {
             }
         }
         System.out.print(separator);
-    }
-
-    public static void updateDatabase(){
-        List<String[]> booksRecords = Book.toCsvPattern(books);
-        Csv.write(booksRecords, DATABASE_PATH);
     }
 
 }
