9c173e2d2d2b9dcaf67a1d7b5364ee7fd3a9447b
jeanarthur <jean.arthursa@gmail.com>
2023-05-14 18:17:35 -0300

    adiciona código base do exercício 03 de FPOO

---
+++ Programacao_de_Tipos_de_Dados_Avancado/Exercicios/Exercicio_03/src/Principal.java
@@ -0,0 +1,30 @@
+// Código base obtido em <https://github.com/univas/2023-OO/tree/main/Encapsulamento/src> [14/05/2023]
+
+import models.ContaCorrente;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ContaCorrente conta = new ContaCorrente("Marcos", "123", "123");
+
+        System.out.println(conta.getSaldo());
+
+        conta.depositar(100);
+        conta.cheque_especial = 100;
+
+        System.out.println(conta.getSaldo());
+
+        conta.sacar(200);
+
+        System.out.println(conta.getSaldo());
+        System.out.println(conta.cheque_especial);
+
+        conta.depositar(200);
+
+        System.out.println();
+        System.out.println(conta.getSaldo());
+        System.out.println(conta.cheque_especial);
+    }
+
+}
---
+++ Programacao_de_Tipos_de_Dados_Avancado/Exercicios/Exercicio_03/src/models/ContaCorrente.java
@@ -0,0 +1,64 @@
+// Código base obtido em <https://github.com/univas/2023-OO/tree/main/Encapsulamento/src/models> [14/05/2023]
+package models;
+
+import java.util.Random;
+
+public class ContaCorrente {
+    public String titular;
+    private double saldo;
+    public String cpf;
+    public String cartao;
+    public String senha;
+    public double cheque_especial;
+    public double limite_cartao;
+    public int numero;
+    public String agencia;
+    public String[] chave_pix;
+    private double juros_cheque = .2;
+
+    public ContaCorrente(String titular, String cpf, String senha) {
+        this.titular = titular;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.saldo = 0;
+        this.cheque_especial = 0;
+        this.limite_cartao = 0;
+
+        this.agencia = "0001";
+        this.numero = (1000000 % new Random().nextInt()) + 1000;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void depositar(double valor) {
+        if(this.saldo < 0) {
+            valor += this.saldo * (this.juros_cheque);
+            this.cheque_especial += (valor - (this.saldo*-1));
+        }
+
+        this.saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        // verificar se o saldo + cheque permite o saque
+        if(this.saldo + this.cheque_especial >= valor) {
+            if(this.saldo < valor) {
+                this.cheque_especial -= (valor - this.saldo);
+            }
+
+            this.saldo -= valor;
+        }else {
+            System.out.println("Nao pode sacar");
+        }
+    }
+
+    public void transferir(String agencia, int conta, double valor) {
+
+    }
+
+    public void transferirPix(String pix, double valor) {
+
+    }
+}
