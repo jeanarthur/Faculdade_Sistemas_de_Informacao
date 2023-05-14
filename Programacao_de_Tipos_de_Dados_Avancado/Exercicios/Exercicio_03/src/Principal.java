ee021d20ca5e5e49243635aeac659d891739089c
jeanarthur <jean.arthursa@gmail.com>
2023-05-14 19:47:17 -0300

    adiciona solução do exercicio_03 de FPOO

--- Programacao_de_Tipos_de_Dados_Avancado/Exercicios/Exercicio_03/src/Principal.java
+++ Programacao_de_Tipos_de_Dados_Avancado/Exercicios/Exercicio_03/src/Principal.java
@@ -5,26 +5,29 @@ import models.ContaCorrente;
 public class Principal {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        ContaCorrente conta = new ContaCorrente("Marcos", "123", "123");
+        ContaCorrente conta1 = new ContaCorrente("Joelha", "403.576.040-40", "12345");
+        ContaCorrente conta2 = new ContaCorrente("Abizagui", "868.376.120-77", "54321");
 
-        System.out.println(conta.getSaldo());
+        System.out.println(conta1.getTitular());
+        conta1.depositar(1000);
+        conta1.sacar(300);
+        conta1.setChequeEspecial(200);
+        conta1.transferir(conta2.getAgencia(), conta2.getNumero(), 700);
+        conta1.sacar(100);
+        conta1.sacar(200);
+        conta1.depositar(300); //Nao pode sacar
 
-        conta.depositar(100);
-        conta.cheque_especial = 100;
+        System.out.println("\n-----CONTAS e EXTRATOS-----");
+        System.out.println(conta1.toString());
+        conta1.getExtrato();
+        System.out.println("--------\n");
 
-        System.out.println(conta.getSaldo());
+        System.out.println(conta2.getTitular());
+        conta2.sacar(300);
+        conta2.depositar(100);
+        System.out.println(conta2.toString());
+        conta2.getExtrato();
 
-        conta.sacar(200);
-
-        System.out.println(conta.getSaldo());
-        System.out.println(conta.cheque_especial);
-
-        conta.depositar(200);
-
-        System.out.println();
-        System.out.println(conta.getSaldo());
-        System.out.println(conta.cheque_especial);
     }
 
 }
